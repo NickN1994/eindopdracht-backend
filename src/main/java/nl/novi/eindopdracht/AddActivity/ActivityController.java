6f1d92259a9817a4c62d6888905242b4f12f0656
NickN1994 <nickneggers@hotmail.com>
2024-01-06 10:56:56 +0100

    database addactivity gemaakt

--- src/main/java/nl/novi/eindopdracht/AddActivity/ActivityController.java
+++ src/main/java/nl/novi/eindopdracht/AddActivity/ActivityController.java
@@ -1,5 +1,8 @@
 package nl.novi.eindopdracht.AddActivity;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,11 @@ public class ActivityController {
 
     public ActivityController(ActivityService activityService) {
         this.activityService = activityService;
+    }
+
+    @PostMapping("/add-activity")
+    public ResponseEntity<ActivityOutputDto> addActivity (@RequestBody ActivityInputDto activityInputDto) {
+        ActivityOutputDto dto = activityService.addActivity(activityInputDto);
+        return ResponseEntity.created(null).body(dto);
     }
 }
--- src/main/java/nl/novi/eindopdracht/AddActivity/ActivityService.java
+++ src/main/java/nl/novi/eindopdracht/AddActivity/ActivityService.java
@@ -11,4 +11,36 @@ public class ActivityService {
     public ActivityService(ActivityRepository activityRepository) {
         this.activityRepository = activityRepository;
     }
+
+    public ActivityOutputDto addActivity(ActivityInputDto dto) {
+        Activity activity = transferToActivity(dto);
+        activityRepository.save(activity);
+        return transferToDto(activity);
+    }
+
+    public Activity transferToActivity (ActivityInputDto dto) {
+        Activity activity = new Activity();
+        activity.setName(dto.getName());
+        activity.setParticipants(dto.getParticipants());
+        activity.setTeacher(dto.getTeacher());
+        activity.setDate(dto.getDate());
+        activity.setTime(dto.getTime());
+        activity.setActivityInfo(dto.getActivityInfo());
+
+        return activity;
+    }
+
+    public ActivityOutputDto transferToDto (Activity activity) {
+        ActivityOutputDto dto = new ActivityOutputDto();
+
+        dto.setName(activity.getName());
+        dto.setParticipants(activity.getParticipants());
+        dto.setTeacher(activity.getTeacher());
+        dto.setDate(activity.getDate());
+        dto.setTime(activity.getTime());
+        dto.setActivityInfo(activity.getActivityInfo());
+
+        return dto;
+    }
+
 }
